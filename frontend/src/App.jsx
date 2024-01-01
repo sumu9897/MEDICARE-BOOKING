81ffff3905f5283113e789ca0802d3592b740320
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-01 21:29:23 +0600

    Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './app.css';
+import Layout from './layouts/Layout';
 
 function App() {
   
 
   return (
-    <>
-      <h1 className="text-[20px] text-blue-600">Hello react +vite app</h1>
-    </>
-  )
+  
+      <Layout/>
+    
+  );
 }
 
 export default App
