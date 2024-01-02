9938be8e9bdb7d53b5d5a768e0a976b7ca016ebf
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-02 09:51:38 +0600

    MEDICARE BOOKING

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,57 @@
+import { useEffect, useRef } from 'react';
+import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
+import logo from '../../assets/images/logo.png';
 
-
-import React from 'react'
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+  {
+    path: '/doctors',
+    display: 'Find a Doctor',
+  },
+  {
+    path: '/services',
+    display: 'Services',
+  },
+  {
+    path: '/contact',
+    display: 'Contact',
+  },
+];
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* ====== logo ====== */}
+          <img src={logo} alt="" />
+        </div>
 
-export default Header
+        {/* ====== menu ====== */}
+        <div className="navigation">
+          <ul className="menu flex items-center gap-[2.7rem]">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  className={(navClass) =>
+                    navClass.isActive
+                      ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                      : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+                  }
+                >
+                  {link.display}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
